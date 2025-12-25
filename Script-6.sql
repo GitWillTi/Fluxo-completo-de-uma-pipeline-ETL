ed9474f393b7c7aa446f59c906a8def29b8d1107
Will <alveswillian198@email.com>
2025-12-25 16:37:35 -0300

    Script-6.sql

---
+++ Script-6.sql
@@ -0,0 +1,9 @@
+create table Povertyano (
+CountryName varchar (255),
+"ano" Integer,
+"Improved water source, urban (% of urban population with access)" Double,
+"Improved water source (% of population with access)" Double,
+"Improved sanitation facilities (% of population with access)" Double,
+"Improved sanitation facilities, rural (% of rural population with access)" Double,
+"Improved sanitation facilities, urban (% of urban population with access)" Double
+)
